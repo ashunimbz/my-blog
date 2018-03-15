231269b44bd2c4f9d0330ad651ab44d971f3a438
ashu <ashu.nim15@gmail.com>
2018-03-15 12:13:16 +0530

    ssecond

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/ashu/djangogirls/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
