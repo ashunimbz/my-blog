8a5851f114a4826468d843cf26bebbdd159ef990
ashu <ashu.nim15@gmail.com>
2018-03-19 16:05:34 +0530

    added

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,11 +51,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/ashu/djangogirls/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
