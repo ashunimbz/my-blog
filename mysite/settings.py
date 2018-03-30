053bdbb13ad857001ad9f5f77be2fc4c5e624773
ashish <ashu.nim15@gmail.com>
2018-03-30 14:32:12 +0530

    added image

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+STATIC_ROOT = "/home/ashunimbz/ashunimbz.pythonanywhere.com/static"
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, "static"),
     
-]
+#]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
